08df074bcde9923b86de799bcd9b47f9e6d7bada
giggityfiles <giggityfiles@gmail.com>
2025-09-11 13:03:46 -0400

    Create giggityad.bat

---
+++ giggityad.bat
@@ -0,0 +1,3 @@
+@echo off
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v LegalNoticeCaption /t REG_SZ /d "giggity files is superior" /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v LegalNoticeText /t REG_SZ /d "xbox live" /f
